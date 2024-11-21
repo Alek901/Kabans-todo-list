f507ed2f0bc7d1a44d7f17fd622e16f2fc6e256b
Alek Gonzalez <Magicguys1@icloud.com>
2024-11-21 13:04:38 -0700

    added a line of code to the authAPI
    Added a fetch to get from the login

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -1,6 +1,7 @@
 import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
+  const  response = await fetch('/api/auth/login')
   // TODO: make a POST request to the login route
 }
 
