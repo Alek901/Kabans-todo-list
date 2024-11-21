4873b199bbf6be448d545b52f2d315ea0b4797a5
Alek Gonzalez <Magicguys1@icloud.com>
2024-11-21 13:41:36 -0700

    Updated auth.ts in middleware
    Added the rest of the code and made a string as well as returning an error for any fraud Dave n Buster tokens

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -12,5 +12,13 @@ export const authenticateToken = (req: Request, res: Response, next: NextFunctio
   if (!token) {
     return res.status(401).json({ message: 'No token provided for Dave n Busters' });
   }
+
+  try {
+    const decoded = jwt.verify(token,process.env.JWT_SECRET as string) as JwtPayload;
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: 'Invalid token for Dave n Busters' });
+  }
   // TODO: verify the token exists and add the user data to the request object
 };
