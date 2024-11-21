6fcb053dfb72f9010fc35dcdbb3bee8479a67d04
Alek Gonzalez <Magicguys1@icloud.com>
2024-11-21 14:08:56 -0700

    Updated the auth Routes.ts
    Sent the token and made some minor changes

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -3,12 +3,11 @@ import { User } from '../models/user.js';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 
-export const login = async (req: Request, res: Response) => { 
-  const { username, password } = req. body;
+export const login = async (req: Request, res: Response) => {
+  const { username, password } = req.body;
 
   try {
     const user = await User.findOne({ where: { username } });
-  }
 
   if (!user) {
     return res.status(400).json({ message: 'User not found' });
@@ -26,8 +25,12 @@ export const login = async (req: Request, res: Response) => {
     { expiresIn: '1h' }
   );
 
-  // TODO: If the user exists and the password is correct, return a JWT token
+   res.json({ token, user: { id: user.id, username: user.username } });
+  } catch (error) {
+    res.status(500).json({ message: 'Server error' });
+  }
 };
+  // TODO: If the user exists and the password is correct, return a JWT token
 
 const router = Router();
 
